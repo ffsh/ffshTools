f9d41a7730e51bbb89a26150a0b697e008edc11c
Benjamin Brahmer <info@b-brahmer.de>
2018-04-20 14:29:40 +0200

    new, better stuff

---
+++ pyDNSCheck/dnsresult.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Holds DNS Resulsts """
+import pprint
+class DNSResult():
+    """ Holds DNS Resulsts """
+
+    def __init__(self):
+        self.results = {
+            "map.freifunk-suedholstein.de": {
+                "1.2.3.4": ["viehbach", "hopfenbach"]
+            }
+        }
+
+    def add(self, domain, hostname, ip_address):
+        """ add a entry to the dns results """
+        if domain in self.results.keys():
+            # domain exists
+            if ip_address in self.results[domain].keys():
+                # ip exists
+                temp = self.results[domain][ip_address]
+                temp.append(hostname)
+                self.results[domain][ip_address] = temp
+            else:
+                # ip does not exist create new ip_address entry
+                self.results[domain][ip_address] = [hostname]
+        else:
+            # domain does not exist create new domain
+            temp = {}
+            temp[ip_address] = [hostname]
+            self.results[domain] = temp
+
+    def get_all(self):
+        """resturns all results"""
+        if __name__ == '__main__':
+            pprint.pprint(self.results)
+        return self.results
+
+def main():
+    """just for testing this module"""
+    dns_results = DNSResult()
+    dns_results.add("map.freifunk-suedholstein.de", "beste", "1.2.3.4")
+    dns_results.add("map.freifunk-suedholstein.de", "barnitz", "1.2.3.5")
+    dns_results.add("map.freifunk-stormarn.de", "barnitz", "1.2.3.6")
+    dns_results.add("map.freifunk-stormarn.de", "trave", "1.2.3.6")
+
+    dns_results.get_all()
+if __name__ == '__main__':
+    main()
--- pyDNSCheck/tester.py
+++ pyDNSCheck/tester.py
@@ -4,54 +4,35 @@ Checks if dns servers in the Freifunk Südholstein Network are consistent
 """
 import json
 import dns.resolver
-import collections
+import dnsresult
 
 class DnsChecker():
     """Query each server defined in self.dns_resolvers for each domain in self.targets"""
     def querry(self, resolvers, targets):
         """performs a nice querry"""
         resolver = dns.resolver.Resolver()
-        results = {}
         test_resolvers = {}
+        dnsresults = dnsresult.DNSResult()
+        # Convert DNS Server list to ip -> name dict
         for name, value in sorted(resolvers["Servers"].items()):
             if value["active"] is True:
                 test_resolvers[value["IPv6"]] = name
+
+        # For each domain in targets query each dns server
         for [domain, record] in sorted(targets["Targets"]):
-            results[domain] = {}
-            results[domain][record] = {}
-            #print("Testing Domain {}:".format(domain))
             for name in sorted(test_resolvers):
                 resolver.nameservers = [name]
                 try:
                     response = resolver.query(domain, record)
-                    for rdata in response:
-                        results[domain][record][test_resolvers[name]] = str(rdata)
-                        #print(test_resolvers[name], rdata, sep=": ")
+                    # get the query result
+                    for ip_address in response:
+                        dnsresults.add(domain, test_resolvers[name], ip_address)
+
                 except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
-                    results[domain][record][test_resolvers[name]] = "None"
-                    #print(test_resolvers[name], "None")
-        with open("something.json", "w") as filep:
-            #print(results)
-            json.dump(results, filep, indent=4)
+                    # No answer or no entry
+                    dnsresults.add(domain, test_resolvers[name], "None")
+        print(dnsresults.get_all())
 
-        self.consistent(results)
-
-    def results(self):
-        """prints pretty results"""
-        pass
-    def consistent(self, results):
-        """will test for consistent stuff"""
-        for key, value in sorted(results.items()):
-            print(key)
-            for key1, value1 in sorted(results[key].items()):
-                value_to_key = collections.defaultdict(list)
-                for key2, value2 in sorted(results[key][key1].items()):
-                    value_to_key[value2].append(key2)
-                for key3, value3 in value_to_key.items():
-                    #if len(value3) == 5:
-                    #    print("ok")
-                    #else:
-                        print(value3, key3, sep=": ")
 
 
 def main():
@@ -61,8 +42,7 @@ def main():
 
     with open("dns-resolvers.json") as file:
         resolvers = json.load(file)
-    #print(targets)
-    #print(resolvers)
+
     checker = DnsChecker()
     checker.querry(resolvers, targets)
 
